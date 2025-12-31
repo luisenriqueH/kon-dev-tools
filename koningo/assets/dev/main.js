06d436afcca18b2d919448e5055a3b9ddc83530c
luisenriqueH <luisenrique@hernandez.com.ve>
2025-12-30 23:59:22 -0400

    base js

--- koningo/assets/dev/main.js
+++ koningo/assets/dev/main.js
@@ -2,3 +2,16 @@ console.log("¡Hola, mundo!");
 console.log("Este es un programa JavaScript sencillo.");
 console.log("Imprime varias líneas en la consola.");
 console.log("Cada línea se imprime usando una declaración console.log separada.");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    console.log("El documento ha sido completamente cargado y analizado.");
+
+    document.querySelector(".entry-content").innerHTML += "<p>¡JavaScript está funcionando correctamente!</p>";
+
+
+
+
+});
